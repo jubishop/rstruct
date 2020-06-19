2ac1c6a4e5bdd5b2377e8509795f7fd17564bf0c
Justin Bishop <jubishop@gmail.com>
2020-06-18 20:00:54 -0700

    much more robust error handling in RStruct and KVStruct

--- lib/rstruct.rb
+++ lib/rstruct.rb
@@ -1,22 +1,48 @@
+require 'set'
+
+require 'core'
+
 class KVStruct < Struct
   def self.new(*args, &block)
     required = args.last.is_a?(Array) ? args[0...-1] : args
+    optional = args.last.is_a?(Array) ? args.last : []
+    argument_intersection = required.intersection(optional)
+    unless argument_intersection.empty?
+      raise ArgumentError, 'Required and optional keys to overlap: ' \
+        "#{argument_intersection.to_a.sentence}"
+    end
+
     super(*args.flatten, keyword_init: true) {
-      @required = required
+      @required, @optional = required.to_set, optional.to_set
+      @all_arguments = @required + optional
       def self.required # rubocop:disable Lint/NestedMethodDefinition
         return @required
+      end
+
+      def self.optional # rubocop:disable Lint/NestedMethodDefinition
+        return @optional
+      end
+
+      def self.all_arguments # rubocop:disable Lint/NestedMethodDefinition
+        return @all_arguments
       end
       class_eval(&block) unless block.nil?
     }
   end
 
   def initialize(args = {}) # rubocop:disable Style/OptionHash
-    self.class.required.each { |param|
-      unless args.key?(param) || args.key?(param.to_s)
-        raise ArgumentError, "Required param: #{param} is missing from " \
-          "#{self.class} constructor."
-      end
-    }
+    missing_required = self.class.required - args.keys
+    unless missing_required.empty?
+      raise ArgumentError, "Required keys: #{missing_required.to_a.sentence} " \
+        "were not passed to #{self.class}.new."
+    end
+
+    if args.keys.length > self.class.all_arguments.size
+      unexpected_params = args.keys.to_set - self.class.all_arguments
+      raise ArgumentError, 'Unexpected keys: ' \
+        "#{unexpected_params.to_a.sentence} were passed to #{self.class}.new."
+    end
+
     super(args)
   end
 end
@@ -24,20 +50,23 @@ end
 class RStruct < Struct
   def self.new(*args, &block)
     num_required = args.last.is_a?(Array) ? args.length - 1 : args.length
+    num_possible = args.flatten.length
+
     super(*args.flatten, keyword_init: false) {
-      @num_required = num_required
-      def self.num_required # rubocop:disable Lint/NestedMethodDefinition
-        return @num_required
+      @param_range = (num_required..num_possible)
+      def self.param_range # rubocop:disable Lint/NestedMethodDefinition
+        return @param_range
       end
       class_eval(&block) unless block.nil?
     }
   end
 
   def initialize(*args)
-    if args.length < self.class.num_required
-      raise ArgumentError, "#{self.class.num_required} params required in " \
-        "#{self.class} constructor."
+    unless self.class.param_range.include?(args.length)
+      raise ArgumentError, "#{self.class.param_range} params must be passed " \
+        "to #{self.class}.new."
     end
+
     super(*args)
   end
 end
--- rstruct.gemspec
+++ rstruct.gemspec
@@ -10,4 +10,5 @@ Gem::Specification.new do |spec|
   spec.required_ruby_version = Gem::Requirement.new(">= 2.7.1")
   spec.metadata["source_code_uri"] = "https://github.com/jubishop/rstruct"
   spec.files         = Dir["lib/**/*.rb"]
+  spec.add_runtime_dependency 'core'
 end
